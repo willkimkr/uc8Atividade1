4a9d41965979f430f45545c78b17f2e1a0205879
willkimkr <willkimkr@outlook.com>
2022-04-22 14:34:23 -0300

    uc8Atividade1

---
+++ evento.js
@@ -0,0 +1,32 @@
+let DataAtual = (19/04/2022)
+let DataEvento = (21/04/2022)
+
+if (DataAtual > DataEvento) {
+    console.log("Cadastro nao Permitido por Data Invalida")
+}else {
+    console.log("Cadastro Permitido")
+}
+
+// Participante for maior de 18 anos
+
+let ParticipanteIdade = (10)
+
+
+if (ParticipanteIdade >= 18) {
+    console.log("Permitir Cadastro")
+}else {
+    console.log("Cadastro nao Permitido Pela Idade")
+}
+
+
+// Lista Participantes <= 100 ("Permitir Cadastro") else ("Cadastro nao Permitido Exedido o Limite")
+
+let QuantidadeParticipantes = 99
+
+if (QuantidadeParticipantes < 100) {
+    console.log("Permitir Cadastro")
+}else {
+    console.log("Excedido o Limite")
+}
+
+
